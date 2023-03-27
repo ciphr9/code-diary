a931f76507f560f680338d7111c03c6a73aec6ef
ciphr9 <dtb.brandl@live.de>
2023-03-28 01:25:09 +0200

    removed cat placeholder

--- src/components/Homepage/Pet.tsx
+++ src/components/Homepage/Pet.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Pet.scss";
 
 function Pet(): JSX.Element {
-  return <div className={"Pet"}>CAT</div>;
+  return <div className={"Pet"}></div>;
 }
 
 export default Pet;
