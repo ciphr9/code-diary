885afb6854ad576c9c3b0cf3f7c01a174eb423de
ciphr9 <dtb.brandl@live.de>
2023-03-28 10:55:55 +0200

    code themes changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer/themes/vsLight");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
